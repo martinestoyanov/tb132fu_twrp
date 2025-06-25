d2270c07864c0bd46ce56645eef1cf7dabc8b9ad
Martin Egor Stoyanov <68874963+martinestoyanov@users.noreply.github.com>
2025-06-25 09:53:05 -0700

    Delete LENOVO/TB132FU directory

--- LENOVO/TB132FU/vendorsetup.sh
+++
@@ -1,9 +0,0 @@
-#
-# Copyright (C) 2025 The Android Open Source Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-add_lunch_combo twrp_TB132FU-user
-add_lunch_combo twrp_TB132FU-userdebug
-add_lunch_combo twrp_TB132FU-eng
-
