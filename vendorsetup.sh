5dfae9ec542a969d5e4461b26ce510d74018745a
Martin Egor Stoyanov <68874963+martinestoyanov@users.noreply.github.com>
2025-06-25 10:35:02 -0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,6 +3,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-add_lunch_combo twrp_TB132FU-user
-add_lunch_combo twrp_TB132FU-userdebug
-add_lunch_combo twrp_TB132FU-eng
+#add_lunch_combo twrp_TB132FU-user
+#add_lunch_combo twrp_TB132FU-userdebug
+#add_lunch_combo twrp_TB132FU-eng
